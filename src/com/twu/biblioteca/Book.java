79ae9bd8701d8f65330dd67c8b02f26df8635142
Ricardo Gouveia <ricardo.gouveia@thoughtworks.com>
2021-02-08 17:13:10 -0300

    getBookSummary w title n year

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,10 +1,32 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class Book {
     private String title;
+    private ArrayList<String> authors;
+    private String yearPublished;
 
     public Book(String title) {
         this.title = title;
+    }
+
+    public Book(String title, ArrayList<String> authors, String yearPublished) {
+        this.title = title;
+        this.authors = authors;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getSummary() {
+        String summaryString = "Book ";
+
+        summaryString = summaryString + title;
+
+         if (yearPublished != "") {
+             summaryString = summaryString + " published on " + yearPublished;
+         }
+
+        return summaryString;
     }
 
     public String getTitle() {
--- test/com/twu/biblioteca/BookTests.java
+++ test/com/twu/biblioteca/BookTests.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class BookTests {
@@ -11,5 +14,20 @@ public class BookTests {
         book = new Book("title");
 
         assertEquals( "title", book.getTitle() );
+    }
+
+    @Test
+    public void getBookSummary() {
+        ArrayList<String> authorsList = new ArrayList<String>();
+        authorsList.add("author 1");
+        authorsList.add("author 2");
+
+        book = new Book("title", authorsList, "2001");
+
+        assertEquals(
+                // "Book title of author1, author2 published on 2001\n",
+                "Book title published on 2001",
+                book.getSummary()
+        );
     }
 }
