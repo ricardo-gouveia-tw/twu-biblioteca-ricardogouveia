ef48394416026c9a92409237bb6e5d6faae11dc0
Ricardo Gouveia <ricardo.gouveia@thoughtworks.com>
2021-02-08 14:03:04 -0300

    Initial Commit

---
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void compare1with1() {
+        assertEquals(1, 1);
+    }
+}
---
+++ test/com/twu/biblioteca/WelcomeUserTest.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+public class WelcomeUserTest {
+    @Test
+    public void userWelcomeStringTest() {
+
+    }
+}
